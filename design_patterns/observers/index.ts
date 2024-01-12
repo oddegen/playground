9b758745acb4ee8de5288aabddf2b5daa40acd23
oddegen <nahomtensae@gmail.com>
2024-01-12 19:44:42 +0300

    refactor: add factory method and remove classes

--- design_patterns/observers/index.ts
+++ design_patterns/observers/index.ts
@@ -1,78 +1,81 @@
-interface observable {
-  subscribe(observer: observer): void;
-  unsubscribe(observer: observer): void;
-  notify(): void;
+/** 
+  Observables are declarative. You define a function for publishing values — the source — but that function is not executed until 
+  a consumer subscribes to the observable by calling the observable's subscribe method. This subscriber then receives notifications 
+  from the observable until it completes, emits an error, or the consumer unsubscribes. An observable can deliver multiple values of 
+  any type — literals, messages, or events — depending on the context. A stream of keystrokes, an HTTP response, and the ticks of an 
+  interval timer are among the typical observable sources. 
+*/
+
+interface _Observable<T> {
+  subscribe(...observer: Partial<_Observer<T>>[]): void;
+  unsubscribe(observer: Partial<_Observer<T>>, initialUnsubscribe: () => void, ...rest: Array<() => void>): void;
+  emit(): void;
 }
 
-interface observer {
-  update(observable: observable): void;
+interface _Observer<T> {
+  onEmit(value: T): void;
+  onError(error: any): void;
 }
 
-class concreteObservable implements observable {
-  private state: number = 0;
-  private observers: observer[];
+const Observable = <T>(state: T, ...observer: Partial<_Observer<T>>[]): _Observable<T> => {
+  let observers = [...observer];
 
-  constructor(observers: observer[]) {
-    this.observers = observers;
-  }
+  return {
+    subscribe(...observer) {
+      observers.push(...observer);
+    },
 
-  subscribe(observer: observer): void {
-    const doesExist = this.observers.find((o) => o === observer);
+    unsubscribe(observer, initialUnsubscribe, ...rest) {
+      if (observers.find((o) => o === observer)) {
+        let errors: any[] | undefined;
 
-    if (doesExist) {
-      console.log("Observer Exists");
-    } else {
-      this.observers.push(observer);
-      console.log("Added Observer");
-    }
-  }
+        try {
+          initialUnsubscribe();
+        } catch (error) {
+          errors = [error];
+        }
 
-  unsubscribe(observer: observer): void {
-    const doesExist = this.observers.find((o) => o === observer);
+        if (rest) {
+          for (const f of rest) {
+            try {
+              f();
+            } catch (error) {
+              errors = errors ?? [];
+              errors.push(error);
+            }
+          }
+        }
 
-    if (doesExist) {
-      this.observers = this.observers.filter((o) => o !== observer);
-      console.log("Removed Observer");
-    } else {
-      console.log("Observer Doesn't Exist");
-    }
-  }
+        if (errors) {
+          throw new Error(
+            `${errors.length} errors occurred during unsubscription:\n${errors
+              .map((err, i) => `${i + 1}) ${err.message}`)
+              .join("\n")}`
+          );
+        }
+      }
+    },
+    emit() {
+      observers.forEach((observer) => {
+        let observe = Observer(observer);
+        try {
+          observe.onEmit(state);
+        } catch (err) {
+          observe.onError(err);
+        }
+      });
+    },
+  };
+};
 
-  notify(): void {
-    console.log("Notifying Observers:");
-    for (const observer of this.observers) {
-      observer.update(this);
-    }
-  }
-
-  updateState(): void {
-    ++this.state;
-    this.notify();
-  }
-
-  getState(): number {
-    return this.state;
-  }
-}
-
-class concreteObserver implements observer {
-  update(observable: concreteObservable): void {
-    console.log("Observer reacting to state change to:", observable.getState());
-  }
-}
-
-const observable = new concreteObservable([] as observer[]);
-const observer_1 = new concreteObserver();
-const observer_2 = new concreteObserver();
-
-observable.subscribe(observer_1);
-observable.subscribe(observer_2);
-
-observable.updateState();
-
-observable.unsubscribe(observer_1);
-
-observable.updateState();
-
-observable.unsubscribe(observer_1);
-observable.unsubscribe(observer_2);
+const Observer = <T>(observerLike: Partial<_Observer<T>>): _Observer<T> => {
+  return {
+    onEmit(value) {
+      console.log("Value:", value);
+    },
+    onError(error) {
+      console.log("Error", error.message);
+    },
+    ...observerLike,
+  };
+};
